db8fcac76ea833bdfc85137d1c5af214dbfb958e
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-10-27 19:05:02 +0100

    install gem simple form

--- cookieslist/app/controllers/cookies_controller.rb
+++ cookieslist/app/controllers/cookies_controller.rb
@@ -1,2 +1,43 @@
 class CookiesController < ApplicationController
+  before_action :set_cookie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cookies = Cookie.all
+  end
+
+  def show
+  end
+
+  def new
+    @cookie = Cookie.new
+  end
+
+  def create
+    cookie = Cookie.new(cookie_params)
+    cookie.save
+    redirect_to cookies_path
+  end
+
+  def edit
+  end
+
+  def update
+    @cookie.update(cookie_params)
+    redirect_to cookies_path
+  end
+
+  def destroy
+    @cookie.delete
+    redirect_to cookies_path
+  end
+
+  private
+
+  def set_cookie
+    @cookie = Cookie.find(params[:id])
+  end
+
+  def cookie_params
+    params.require(:cookie).permit(:name, :flavour, :note)
+  end
 end
